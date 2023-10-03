a24c0c300d2017137d147fee88e885208612ac11
nungo <nued65@gmail.com>
2023-10-03 03:00:02 +0300

    3n4 fixes

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 number = 98
-Battery_street = "Battery street"
-word = "{} {}\n".format(number, Battery_street)
-print(word)
+print(f"{number} Battery street")
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-al = "Float: {:.2f}\n"
-print(al.format(number))
+print(f"Float:{number:.2f}")
