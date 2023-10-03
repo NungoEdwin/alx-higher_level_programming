b338cb812eb0923e14b7fbbaddfbe619c9b95908
nungo <nued65@gmail.com>
2023-10-04 00:03:26 +0300

    12fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for num in range(1, 101):
-        print("FizzBuzz", end='') if num % 3 == 0 and num % 5 == 0 \
+        print("FizzBuzz ", end='') if num % 3 == 0 and num % 5 == 0 \
                 else print("Fizz ", end='') \
                 if num % 3 == 0 else print("Buzz ", end='') \
                 if num % 5 == 0 else print("{:d} ".format(num), end='')
